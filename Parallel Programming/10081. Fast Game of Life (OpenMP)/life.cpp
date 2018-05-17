26e9151a531c15ffa8f18bf6e9b96df7a9f81200
JCly-rikiu <jcly.rikiu@gmail.com>
2018-05-17 15:25:17 +0800

    modify 10081

--- Parallel Programming/10081. Fast Game of Life (OpenMP)/life.cpp
+++ Parallel Programming/10081. Fast Game of Life (OpenMP)/life.cpp
@@ -1,11 +1,10 @@
-#include <omp.h>
-#include <stdlib.h>
 #include <cstdio>
+#include <cstdlib>
+#include <omp.h>
 
 const int MAXN = 2010;
 
-bool tableA[MAXN][MAXN];
-bool tableB[MAXN][MAXN];
+bool table[2][MAXN][MAXN];
 
 int main() {
   int n, generation;
@@ -15,61 +14,34 @@ int main() {
     char temp[2010];
     scanf("%s", temp);
     for (int j = 1; j <= n; j++)
-      tableA[i][j] = (temp[j - 1] == '1');
+      table[0][i][j] = (temp[j - 1] == '1');
   }
 
 #pragma omp parallel
-  for (int g = 0; g < generation; g++) {
-    int now = g % 2;
-    if (now == 0) {
+  for (int g = 0, now = 0; g < generation; g++, now = 1 - now) {
 #pragma omp for
-      for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++) {
-          int nln = tableA[i + 1][j] + tableA[i - 1][j] + tableA[i][j + 1] + tableA[i][j - 1] + tableA[i + 1][j + 1] + tableA[i + 1][j - 1] + tableA[i - 1][j + 1] + tableA[i - 1][j - 1];
-          if (tableA[i][j]) {
-            if (nln != 2 && nln != 3)
-              tableB[i][j] = false;
-            else
-              tableB[i][j] = true;
-          } else {
-            if (nln == 3)
-              tableB[i][j] = true;
-            else
-              tableB[i][j] = false;
-          }
+    for (int i = 1; i <= n; i++)
+      for (int j = 1; j <= n; j++) {
+        int nln = table[now][i + 1][j] + table[now][i - 1][j] + table[now][i][j + 1] + table[now][i][j - 1] + table[now][i + 1][j + 1] + table[now][i + 1][j - 1] + table[now][i - 1][j + 1] + table[now][i - 1][j - 1];
+        if (table[now][i][j]) {
+          if (nln != 2 && nln != 3)
+            table[1 - now][i][j] = false;
+          else
+            table[1 - now][i][j] = true;
+        } else {
+          if (nln == 3)
+            table[1 - now][i][j] = true;
+          else
+            table[1 - now][i][j] = false;
         }
-    } else {
-#pragma omp for
-      for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++) {
-          int nln = tableB[i + 1][j] + tableB[i - 1][j] + tableB[i][j + 1] + tableB[i][j - 1] + tableB[i + 1][j + 1] + tableB[i + 1][j - 1] + tableB[i - 1][j + 1] + tableB[i - 1][j - 1];
-          if (tableB[i][j]) {
-            if (nln != 2 && nln != 3)
-              tableA[i][j] = false;
-            else
-              tableA[i][j] = true;
-          } else {
-            if (nln == 3)
-              tableA[i][j] = true;
-            else
-              tableA[i][j] = false;
-          }
-        }
-    }
+      }
   }
 
-  if (generation % 2 == 0) {
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= n; j++)
-        printf("%c", tableA[i][j] ? '1' : '0');
-      printf("\n");
-    }
-  } else {
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= n; j++)
-        printf("%c", tableB[i][j] ? '1' : '0');
-      printf("\n");
-    }
+  int now = generation % 2;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n; j++)
+      printf("%c", table[now][i][j] ? '1' : '0');
+    printf("\n");
   }
 
   return 0;
